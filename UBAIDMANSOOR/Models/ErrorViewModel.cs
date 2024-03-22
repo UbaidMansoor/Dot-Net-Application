81ad74ff7ee357bde2cb57c4331d69b576addb3a
iamDaniyalAhmad <SP22BSCS0087@maju.edu.pk>
2024-03-22 19:54:12 +0500

    Add project files.

---
+++ UBAIDMANSOOR/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UBAIDMANSOOR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
